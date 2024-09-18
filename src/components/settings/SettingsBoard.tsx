19353d1a72e177a1488a1dae1d56ec6e5286a8f2
Bulgaashd <bulgantamir.b@ebazaar.mn>
2024-09-18 13:44:13 +0800

    update

--- src/components/settings/SettingsBoard.tsx
+++ src/components/settings/SettingsBoard.tsx
@@ -1,16 +1,9 @@
-import { tr } from '@/lib/utils';
-import { Card, CardBody, CardHeader } from '@nextui-org/react';
 import { FunctionComponent } from 'react';
 
 interface SettingsBoardProps {}
 
 const SettingsBoard: FunctionComponent<SettingsBoardProps> = () => {
-  return (
-    <Card className='h-full'>
-      <CardHeader>{tr('Тохиргоо')}</CardHeader>
-      <CardBody></CardBody>
-    </Card>
-  );
+  return <div>SettingsBoard</div>;
 };
 
 export default SettingsBoard;
