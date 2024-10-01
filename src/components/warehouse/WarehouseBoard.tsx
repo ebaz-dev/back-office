4da3bfb65c0d253df997be250308faed9a04da87
Bulgaashd <bulgantamir.b@ebazaar.mn>
2024-10-01 15:36:55 +0800

    update

--- src/components/warehouse/WarehouseBoard.tsx
+++
@@ -1,9 +0,0 @@
-import { FunctionComponent } from 'react';
-
-interface WarehouseBoardProps {}
-
-const WarehouseBoard: FunctionComponent<WarehouseBoardProps> = () => {
-  return <div>WarehouseBoard</div>;
-};
-
-export default WarehouseBoard;
