3af656005fa0e830220f935f08127a69523e1ac2
Bulgaashd <bulgantamir.b@ebazaar.mn>
2024-10-18 13:42:17 +0800

    update

--- src/app/actions/products.tsx
+++ src/app/actions/products.tsx
@@ -1,6 +1,8 @@
 'use server';
 
+import { ProductsColumns } from '@/lib/columns';
 import { fetcher } from '@/lib/fetch';
+import { IColumn, IProduct } from '@/lib/types';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
@@ -27,6 +29,25 @@ export async function updateProductImageAction(item: any) {
   revalidatePath('/products');
 }
 
-export async function createProductAction(state: any, formData: FormData) {}
+export async function createProductAction(
+  state: { images: string[] },
+  formData: FormData
+) {
+  const fieldOptions = Object.fromEntries(
+    ProductsColumns().map((product: IColumn) => [
+      product.name,
+      formData.get(product.name)
+    ])
+  );
+
+  const reqOptions = {
+    ...fieldOptions,
+    images: state.images
+  };
+
+  const response = await fetcher(`/product/create/`, 'POST', reqOptions);
+
+  console.log(response, reqOptions);
+}
 
 export async function importProductsAction(formData: FormData) {}
