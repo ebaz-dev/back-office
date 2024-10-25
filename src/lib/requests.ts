2ace8ee7211120ecfb025f914373f36209918a92
Bulgaashd <bulgantamir.b@ebazaar.mn>
2024-10-25 13:49:27 +0800

    update

--- src/lib/requests.ts
+++ src/lib/requests.ts
@@ -1,13 +1,13 @@
 import { fetcher } from '@/lib/fetch';
 
 export const getOrders = async (currentParams: string) =>
-  await fetcher(`/order/bo/list?${currentParams}&page=1&limit=10`);
+  await fetcher(`/order/bo/list?${currentParams}&limit=10`);
 
 export const getProducts = async (currentParams: string) =>
-  await fetcher(`/product/dashboard/list?${currentParams}&page=1&limit=10`);
+  await fetcher(`/product/dashboard/list?${currentParams}&limit=10`);
 
 export const getCustomers = async (currentParams: string) =>
-  await fetcher(`/customer/list?${currentParams}&page=1&limit=10`);
+  await fetcher(`/customer/list?${currentParams}&limit=10`);
 
 export const getProductBrands = async (supplierId: string) =>
   await fetcher(`/product/brands?customerId=${supplierId}&limit=all`);
