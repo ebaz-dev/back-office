9f098237e2a695f50ebcea508113c1ac51a7761d
sukhtulga <sukhtulga@tomyo.mn>
2024-10-29 14:24:49 +0800

    provider refactor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,29 @@
-import type { Metadata } from 'next';
-import '@/styles/globals.css';
-import 'swiper/css';
-import 'swiper/css/effect-coverflow';
-import 'swiper/css/pagination';
-import { NextUIProvider } from '@/providers/NextUIProvider';
-import { NextThemeProvider } from '@/providers/NextThemeProvider';
+import type { Metadata } from "next";
+import "@/styles/globals.css";
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import { NextUIProvider } from "@nextui-org/react";
+import { ThemeProvider } from "next-themes";
 
 export const metadata: Metadata = {
-  title: 'E-bazaar',
-  description: 'Нэгдсэн хялбар захиалга',
-  icons: '/logos/logo-symbol.svg'
+  title: "E-bazaar",
+  description: "Нэгдсэн хялбар захиалга",
+  icons: "/logos/logo-symbol.svg",
 };
 
 export default function RootLayout({
-  children
+  children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en' suppressHydrationWarning>
-      <body className='bg-default-100'>
+    <html lang="en" suppressHydrationWarning>
+      <body className="bg-default-100">
         <NextUIProvider>
-          <NextThemeProvider>{children}</NextThemeProvider>
+          <ThemeProvider attribute="class" defaultTheme="light">
+            {children}
+          </ThemeProvider>
         </NextUIProvider>
       </body>
     </html>
