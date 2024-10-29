64c3f4fa36a792793cf5283839269301baae8b2a
sukhtulga <sukhtulga@tomyo.mn>
2024-10-29 14:25:18 +0800

    next auth refactor

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,27 +1,27 @@
-import { getCustomers } from '@/lib/requests';
-import NavProvider from '@/providers/NavProvider';
-import NextAuthProvider from '@/providers/NextAuthProvider';
-import { FunctionComponent, ReactNode } from 'react';
-import { getCookie } from '@/app/actions/cookies';
+import { getCustomers } from "@/lib/requests";
+import NavProvider from "@/providers/NavProvider";
+import { FunctionComponent, ReactNode } from "react";
+import { getCookie } from "@/app/actions/cookies";
+import { SessionProvider } from "next-auth/react";
 
 interface MainLayoutProps {
   children: ReactNode;
 }
 
 const MainLayout: FunctionComponent<MainLayoutProps> = async ({ children }) => {
-  const chosenSupplier = await getCookie('supplier');
+  const chosenSupplier = await getCookie("supplier");
 
-  const suppliers = await getCustomers('type=supplier');
+  const suppliers = await getCustomers("type=supplier");
 
   return (
-    <NextAuthProvider>
+    <SessionProvider>
       <NavProvider
         suppliers={suppliers?.data ?? []}
         chosenSupplier={chosenSupplier ? JSON.parse(chosenSupplier) : null}
       >
         {children}
       </NavProvider>
-    </NextAuthProvider>
+    </SessionProvider>
   );
 };
 
--- src/app/actions/auth.tsx
+++ src/app/actions/auth.tsx
@@ -1,40 +1,43 @@
-'use server';
+"use server";
 
-import { signIn, signOut } from '@/auth';
-import { LoginFormSchema, LoginFormState } from '@/lib/definitions';
-import { loginFetch } from '@/lib/fetch';
-import { deleteCookie } from '@/app/actions/cookies';
+import { signIn, signOut } from "@/config/auth";
+import { LoginFormSchema, LoginFormState } from "@/lib/definitions";
+import { loginFetch } from "@/lib/fetch";
+import { deleteCookie } from "@/app/actions/cookies";
 
 export async function loginAction(state: LoginFormState, formData: FormData) {
   const validatedFields = LoginFormSchema.safeParse({
-    username: formData.get('username'),
-    password: formData.get('password')
+    username: formData.get("username"),
+    password: formData.get("password"),
   });
 
   const { success, error, data } = validatedFields;
 
   if (!success) {
     return {
-      errors: error.flatten().fieldErrors
-    };
-  } else {
-    const user = await loginFetch(data.username, data.password);
-
-    if (user && user.id) {
-      await signIn('credentials', {
-        userId: user.id,
-        redirectTo: '/dashboard'
-      });
-    }
-
-    return {
-      message: user?.errors[0].message
+      errors: error.flatten().fieldErrors,
     };
   }
+
+  const { username, password } = data;
+  const response = await loginFetch(username, password);
+
+  const { id, errors } = response;
+
+  if (id) {
+    await signIn("credentials", {
+      userId: id,
+      redirectTo: "/dashboard",
+    });
+  }
+
+  return {
+    message: errors[0].message,
+  };
 }
 
 export async function logoutAction() {
-  deleteCookie('supplier'),
-    deleteCookie('session'),
-    await signOut({ redirectTo: '/login' });
+  deleteCookie("supplier"),
+    deleteCookie("session"),
+    await signOut({ redirectTo: "/login" });
 }
