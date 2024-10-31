12b25bccbe98e0b89bbeb0cd0886d07e15868c8b
sukhtulga <sukhtulga@tomyo.mn>
2024-10-31 14:49:46 +0800

    refactor: 💡 Type and build fix

--- src/services/merchant.service.ts
+++ src/services/merchant.service.ts
@@ -15,5 +15,5 @@ export async function getMerchants(
     supplierId,
   };
 
-  return dataProvider.getList<any[]>("/customer/bo/list", params);
+  return dataProvider.getList<IMerchant[]>("/customer/bo/list", params);
 }
