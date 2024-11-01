19a2b13beca80d3e888203c2abff480ab810a65a
sukhtulga <sukhtulga@tomyo.mn>
2024-11-01 09:41:55 +0800

    feat: 🎸 role permission store on supplier selection

--- src/components/core/CoreSelectSupplier.tsx
+++ src/components/core/CoreSelectSupplier.tsx
@@ -1,24 +1,29 @@
-import { chooseSupplierAction, removeSupplierAction } from '@/app/actions/main';
-import { ICustomer } from '@/types/customer.types';
-import { tr } from '@/lib/utils';
-import { Autocomplete, AutocompleteItem } from '@nextui-org/react';
-import { usePathname } from 'next/navigation';
-import { FunctionComponent, Key } from 'react';
+import { chooseSupplierAction, removeSupplierAction } from "@/app/actions/main";
+import { ICustomer } from "@/types/customer.types";
+import { tr } from "@/lib/utils";
+import { Autocomplete, AutocompleteItem } from "@nextui-org/react";
+import { usePathname } from "next/navigation";
+import { FunctionComponent, Key } from "react";
+import { useAppDispatch } from "@/lib/hooks";
+import { fetchRoleData } from "@/lib/reducers/roleReducer";
 
 interface CoreSelectSupplierProps {
   suppliers: ICustomer[];
   chosenSupplier: ICustomer;
 }
 
-const CoreSelectSupplier: FunctionComponent<
-  CoreSelectSupplierProps
-> = props => {
+const CoreSelectSupplier: FunctionComponent<CoreSelectSupplierProps> = (
+  props
+) => {
   const { suppliers, chosenSupplier } = props;
 
   const pathname = usePathname();
+  const dispatch = useAppDispatch();
+
+  dispatch(fetchRoleData(chosenSupplier?.id));
 
   const onSelectionChange = (key: Key | null) => {
-    const findSupplier = suppliers.find(supplier => supplier.id === key);
+    const findSupplier = suppliers.find((supplier) => supplier.id === key);
 
     if (findSupplier && key) {
       chooseSupplierAction(findSupplier, pathname);
@@ -31,24 +36,24 @@ const CoreSelectSupplier: FunctionComponent<
 
   return (
     <Autocomplete
-      className='max-w-xs'
+      className="max-w-xs"
       defaultSelectedKey={chosenSupplier?.id}
       defaultItems={suppliers}
-      color='primary'
-      label={tr('-- Нийлүүлэгч сонгох --')}
-      variant='flat'
-      radius='none'
+      color="primary"
+      label={tr("-- Нийлүүлэгч сонгох --")}
+      variant="flat"
+      radius="none"
       onSelectionChange={onSelectionChange}
       clearButtonProps={{
-        onPress: onClear
+        onPress: onClear,
       }}
       listboxProps={{
         emptyContent: (
-          <span className='text-xs'>{tr('Нийлүүлэгч олдсонгүй')}.</span>
-        )
+          <span className="text-xs">{tr("Нийлүүлэгч олдсонгүй")}.</span>
+        ),
       }}
     >
-      {item => <AutocompleteItem key={item.id}>{item.name}</AutocompleteItem>}
+      {(item) => <AutocompleteItem key={item.id}>{item.name}</AutocompleteItem>}
     </Autocomplete>
   );
 };
