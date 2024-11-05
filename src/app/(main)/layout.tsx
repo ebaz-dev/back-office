c0c3822ec82b8e3acb57f7732893820f39d0f18e
Bulgaashd <bulgantamir.b@ebazaar.mn>
2024-11-05 13:38:13 +0800

    fix build error

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,29 +1,28 @@
-import { getCustomers } from "@/lib/requests";
-import NavProvider from "@/providers/NavProvider";
-import { FunctionComponent, ReactNode } from "react";
-import { getCookie } from "@/app/actions/cookies";
-import { SessionProvider } from "next-auth/react";
-import { StoreProvider } from "@/providers/StoreProvider";
-import { getSuppliers } from "@/services/customer.service";
+import NavProvider from '@/providers/NavProvider';
+import { FunctionComponent, ReactNode } from 'react';
+import { getCookie } from '@/app/actions/cookies';
+import { SessionProvider } from 'next-auth/react';
+import { StoreProvider } from '@/providers/StoreProvider';
+import { getSuppliers } from '@/services/customer.service';
 
 interface MainLayoutProps {
   children: ReactNode;
 }
 
 const MainLayout: FunctionComponent<MainLayoutProps> = async ({ children }) => {
-  const chosenSupplier = await getCookie("supplier");
+  const chosenSupplier = await getCookie('supplier');
   const suppliers = await getSuppliers();
 
   // TODO: fetch user roles
 
   const role = {
-    role: "BACKOFFICE_ADMIN",
+    role: 'BACKOFFICE_ADMIN',
     permissions: {
-      dashboard: ["list", "create", "update", "delete", "show"],
-      merchant: ["list", "create", "update", "delete", "show"],
-      product: ["list", "create", "update", "delete", "show"],
-      order: ["list", "create", "update", "delete", "show"],
-    },
+      dashboard: ['list', 'create', 'update', 'delete', 'show'],
+      merchant: ['list', 'create', 'update', 'delete', 'show'],
+      product: ['list', 'create', 'update', 'delete', 'show'],
+      order: ['list', 'create', 'update', 'delete', 'show']
+    }
   };
 
   return (
